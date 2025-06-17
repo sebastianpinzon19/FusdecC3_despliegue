f07ab0426f2829ce43b238764ba2d220a0644fb7
sebastianpinzon19 <sebastian_pinzonr@soy.sena.edu.co>
2025-06-17 16:03:26 -0500

    despliege

--- FusdecC3-FrontEnd-React/vite.config.js
+++ FusdecC3-FrontEnd-React/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Agrupa las dependencias principales
+          // Agrega más manualChunks según sea necesario
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000, // Ajusta este valor según sea necesario
+  },
 })
